1d6c6fbbf188c67c461e9ce71b999fc806612df2
Tiago Cardoso <tcardoso@gmail.com>
2019-01-05 22:00:24 +0800

    Moved vermon files to this repo

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict"
+
+exports.entities = require('./Entities');
+exports.extensions = require('./Extensions');
+exports.filters = require('./Filters');
+exports.em = require('./EnvironmentManager');
---
+++ test/basic-specs.js
@@ -0,0 +1,32 @@
+/*****************************************************
+ * Basic tests
+ *****************************************************/
+
+let chai = require('chai')
+let chaiAsPromised = require('chai-as-promised')
+let should = chai.should()
+let fs = require('fs')
+let entities = require('../Entities')
+let em = require('../EnvironmentManager')
+let extentions = require('../Extensions')
+let filters = require('../Filters')
+let main = require('../main')
+
+// Chai will use promises for async events
+chai.use(chaiAsPromised)
+
+before(function (done) {
+  done()
+})
+
+after(function (done) {
+  // here you can clear fixtures, etc.
+  done()
+})
+
+describe('Basic tests, ', function () {
+  it('makes sure dependencies are loaded correctly (code does nothing)', function (done) {
+    // Prepare
+    done()
+  })
+})
